ccf5c98625680674d2a91bee954d8dca25da94d3
nursalimdev <nursalimdev123@gmail.com>
2023-02-05 07:57:27 +0700

    commit

---
+++ src/test/java/nursalim/dev/validation/ValidatorTest.java
@@ -0,0 +1,19 @@
+package nursalim.dev.validation;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ValidatorTest {
+    @Test
+    void testCreateValidator(){
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+
+        Assertions.assertNotNull(validator);
+        validatorFactory.close();
+
+    }
+}
